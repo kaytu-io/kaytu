684336917d6bad301a483651b4d7764e73c7d466
artaasadi <arta-c@kaytu.io>
2024-04-26 20:25:04 +0200

    fix: fix chocolateyinstall version

--- scripts/templates/chocolateyinstall.ps1
+++ scripts/templates/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = 'Stop'
-
-$url64 = "https://github.com/kaytu-io/kaytu/releases/download/{PLACEHOLDER_VERSION}/kaytu_{PLACEHOLDER_VERSION}_windows_amd64.zip"
+$version = "{PLACEHOLDER_VERSION}"
+$version = $version.TrimStart('v')
+$url64 = "https://github.com/kaytu-io/kaytu/releases/download/{PLACEHOLDER_VERSION}/kaytu_$($version)_windows_amd64.zip"
 $unzipLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageParams = @{
