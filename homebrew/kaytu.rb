3d0e6527f38584126f8f062e16b26e0a517a6f9d
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-04-27 01:02:07 +0400

    fix: Fixes homebrew

--- homebrew/kaytu.rb
+++ homebrew/kaytu.rb
@@ -7,21 +7,33 @@ class Kaytu < Formula
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_darwin_amd64.tar.gz"
     sha256 "HASH_MAC_AMD64"
+    def install
+      bin.install "kaytu" => "kaytu"
+    end
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_darwin_arm64.tar.gz"
     sha256 "HASH_MAC_ARM64"
+    def install
+      bin.install "kaytu" => "kaytu"
+    end
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_linux_arm64.tar.gz"
     sha256 "HASH_LINUX_ARM64"
+    def install
+      bin.install "kaytu" => "kaytu"
+    end
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_linux_amd64.tar.gz"
     sha256 "HASH_LINUX_AMD64"
+    def install
+      bin.install "kaytu" => "kaytu"
+    end
   end
 
 
