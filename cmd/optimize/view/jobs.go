e41a53827a391f3a1531ea23f3dd1203f138bc57
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-04-26 21:10:42 +0400

    fix: Fixes status responsiveness

--- cmd/optimize/view/jobs.go
+++ cmd/optimize/view/jobs.go
@@ -2,69 +2,181 @@ package view
 
 import (
 	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/muesli/reflow/wordwrap"
+	"sort"
 	"strings"
+	"sync"
 )
 
+type Job struct {
+	ID             string
+	Descrption     string
+	FailureMessage string
+	Done           bool
+}
+
 type JobsView struct {
+	runningJobsMap map[string]string
+	failedJobsMap  map[string]string
+
 	runningJobs     []string
 	moreRunningJobs bool
 	failedJobs      []string
 	moreFailedJobs  bool
+	statusErr       string
 
 	height int
 	width  int
+
+	jobMutex  sync.RWMutex
+	jobChan   chan Job
+	errorChan chan error
 }
 
-func (h *JobsView) SetWidth(width int) {
-	h.width = width
+func NewJobsView() *JobsView {
+	return &JobsView{
+		runningJobsMap:  map[string]string{},
+		failedJobsMap:   map[string]string{},
+		runningJobs:     nil,
+		moreRunningJobs: false,
+		failedJobs:      nil,
+		moreFailedJobs:  false,
+		statusErr:       "",
+		height:          0,
+		width:           0,
+		jobMutex:        sync.RWMutex{},
+		jobChan:         make(chan Job, 10000),
+		errorChan:       make(chan error, 10000),
+	}
 }
 
-func (h *JobsView) SetHeight(height int) {
-	h.height = height
+func (m *JobsView) Init() tea.Cmd {
+	go m.UpdateStatus()
+	return nil
 }
 
-func (h *JobsView) MinHeight() int {
-	return 1
+func (m *JobsView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	m.jobMutex.RLock()
+	m.runningJobs, m.moreRunningJobs = m.RunningJobs()
+	m.failedJobs, m.moreFailedJobs = m.FailedJobs()
+	m.jobMutex.RUnlock()
+
+	return m, nil
 }
 
-func (h *JobsView) MaxHeight() int {
-	maxFailedLines := len(h.failedJobs)
-	maxRunningLines := len(h.runningJobs)
-	if h.moreRunningJobs {
+func (m *JobsView) RunningJobs() ([]string, bool) {
+	if len(m.runningJobsMap) == 0 {
+		return nil, false
+	}
+	var res []string
+	for _, v := range m.runningJobsMap {
+		res = append(res, v)
+	}
+	sort.Strings(res)
+	count := 3
+	if len(res) < 3 {
+		count = len(res)
+	}
+	return res[:count], len(m.runningJobsMap) > 3
+}
+
+func (m *JobsView) FailedJobs() ([]string, bool) {
+	if len(m.failedJobsMap) == 0 {
+		return nil, false
+	}
+	var res []string
+	for _, v := range m.failedJobsMap {
+		res = append(res, v)
+	}
+	sort.Strings(res)
+	count := 3
+	if len(res) < 3 {
+		count = len(res)
+	}
+	return res[:count], len(m.failedJobsMap) > 3
+}
+
+func (m *JobsView) UpdateStatus() {
+	for {
+		select {
+		case job := <-m.jobChan:
+			m.jobMutex.Lock()
+			if !job.Done {
+				m.runningJobsMap[job.ID] = job.Descrption
+			} else {
+				if _, ok := m.runningJobsMap[job.ID]; ok {
+					delete(m.runningJobsMap, job.ID)
+				}
+			}
+			if len(job.FailureMessage) > 0 {
+				m.failedJobsMap[job.ID] = fmt.Sprintf("%s failed due to %s", job.Descrption, job.FailureMessage)
+			}
+			m.jobMutex.Unlock()
+
+		case err := <-m.errorChan:
+			m.statusErr = fmt.Sprintf("Failed due to %v", err)
+		}
+	}
+}
+
+func (m *JobsView) SetWidth(width int) {
+	m.width = width
+}
+
+func (m *JobsView) SetHeight(height int) {
+	m.height = height
+}
+
+func (m *JobsView) MinHeight() int {
+	statusHeight := 0
+	if len(m.statusErr) > 0 {
+		statusHeight = strings.Count(wordwrap.String("  error: "+m.statusErr, m.width), "\n") + 1
+	}
+	return statusHeight + 1
+}
+
+func (m *JobsView) MaxHeight() int {
+	statusHeight := 0
+	if len(m.statusErr) > 0 {
+		statusHeight = strings.Count(wordwrap.String("  error: "+m.statusErr, m.width), "\n") + 1
+	}
+	maxFailedLines := len(m.failedJobs)
+	maxRunningLines := len(m.runningJobs)
+	if m.moreRunningJobs {
 		maxRunningLines++
 	}
-	if h.moreFailedJobs {
+	if m.moreFailedJobs {
 		maxFailedLines++
 	}
-	return maxRunningLines + maxFailedLines
+	return statusHeight + maxRunningLines + maxFailedLines
 }
 
-func (h *JobsView) IsResponsive() bool {
-	return h.height >= h.MinHeight()
+func (m *JobsView) IsResponsive() bool {
+	return m.height >= m.MinHeight()
 }
 
-func (h *JobsView) String() string {
-	maxFailedLines := len(h.failedJobs)
-	maxRunningLines := len(h.runningJobs)
-	if h.moreRunningJobs {
+func (m *JobsView) View() string {
+	maxFailedLines := len(m.failedJobs)
+	maxRunningLines := len(m.runningJobs)
+	if m.moreRunningJobs {
 		maxRunningLines++
 	}
-	if h.moreFailedJobs {
+	if m.moreFailedJobs {
 		maxFailedLines++
 	}
 	runningShowCount := 0
 	failedShowCount := 0
 
-	for runningShowCount+failedShowCount < h.height {
-		if runningShowCount == 0 && runningShowCount < len(h.runningJobs) {
+	for runningShowCount+failedShowCount < m.height {
+		if runningShowCount == 0 && runningShowCount < len(m.runningJobs) {
 			runningShowCount++
 			continue
 		}
 
-		if failedShowCount < len(h.failedJobs) {
+		if failedShowCount < len(m.failedJobs) {
 			failedShowCount++
-		} else if runningShowCount < len(h.runningJobs) {
+		} else if runningShowCount < len(m.runningJobs) {
 			runningShowCount++
 		} else if failedShowCount < maxFailedLines {
 			failedShowCount++
@@ -76,8 +188,8 @@ func (h *JobsView) String() string {
 	}
 
 	var lines []string
-	if runningShowCount > 0 && len(h.runningJobs) > 0 {
-		for idx, v := range h.runningJobs {
+	if runningShowCount > 0 && len(m.runningJobs) > 0 {
+		for idx, v := range m.runningJobs {
 			if runningShowCount == 0 {
 				break
 			}
@@ -85,15 +197,15 @@ func (h *JobsView) String() string {
 			if idx == 0 {
 				line = fmt.Sprintf(" jobs: - %s", v)
 			}
-			lines = append(lines, wordwrap.String(line, h.width))
+			lines = append(lines, wordwrap.String(line, m.width))
 			runningShowCount--
 		}
-		if h.moreRunningJobs && runningShowCount > 0 {
+		if m.moreRunningJobs && runningShowCount > 0 {
 			lines = append(lines, "       ...")
 		}
 	}
-	if failedShowCount > 0 && len(h.failedJobs) > 0 {
-		for idx, v := range h.failedJobs {
+	if failedShowCount > 0 && len(m.failedJobs) > 0 {
+		for idx, v := range m.failedJobs {
 			if failedShowCount == 0 {
 				break
 			}
@@ -101,13 +213,27 @@ func (h *JobsView) String() string {
 			if idx == 0 {
 				line = fmt.Sprintf(" failures: - %s", v)
 			}
-			lines = append(lines, errorStyle.Render(wordwrap.String(line, h.width)))
+			lines = append(lines, errorStyle.Render(wordwrap.String(line, m.width)))
 			failedShowCount--
 		}
-		if h.moreFailedJobs && failedShowCount > 0 {
+		if m.moreFailedJobs && failedShowCount > 0 {
 			lines = append(lines, errorStyle.Render("       ..."))
 		}
 	}
 
-	return strings.Join(lines, "\n")
+	statusErr := ""
+	if len(m.statusErr) > 0 {
+		statusErr = errorStyle.Render(wordwrap.String("  error: "+m.statusErr, m.width)) + "\n"
+	}
+
+	return statusErr + strings.Join(lines, "\n")
+}
+
+func (m *JobsView) PublishError(err error) {
+	m.errorChan <- err
+}
+
+func (m *JobsView) Publish(job Job) Job {
+	m.jobChan <- job
+	return job
 }
