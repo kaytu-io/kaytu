f1c3a67e5fb3627868a9c3ae9e8d67bc6214bbfe
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-04-26 21:14:56 +0400

    fix: Fixes homebrew

---
+++ homebrew/kaytu.rb
@@ -0,0 +1,43 @@
+class Kaytu < Formula
+  desc "CLI application for Kaytu"
+  homepage "https://github.com/kaytu-io/cli-program"
+  version "VERSION_HOMEBREW"
+  license "MIT"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/kaytu-io/cli-program/releases/download/vVERSION_HOMEBREW/ktucli-macos-amd64"
+    sha256 "HASH_MAC_AMD64"
+    def install
+      bin.install "ktucli-macos-amd64" => "kaytu"
+    end
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/kaytu-io/cli-program/releases/download/vVERSION_HOMEBREW/ktucli-macos-arm64"
+    sha256 "HASH_MAC_ARM64"
+    def install
+      bin.install "ktucli-macos-arm64" => "kaytu"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://github.com/kaytu-io/cli-program/releases/download/vVERSION_HOMEBREW/ktucli-linux-arm64"
+    sha256 "HASH_LINUX_ARM64"
+    def install
+      bin.install "ktucli-linux-arm64" => "kaytu"
+    end
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kaytu-io/cli-program/releases/download/vVERSION_HOMEBREW/ktucli-linux-amd64"
+    sha256 "HASH_LINUX_AMD64"
+    def install
+      bin.install "ktucli-linux-amd64" => "kaytu"
+    end
+  end
+
+
+  test do
+    system "#{bin}/kaytu", "--version"
+  end
+end
