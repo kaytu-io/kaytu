9807e11181e069cb87f50e2958913e58b27b7b04
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-04-27 00:39:59 +0400

    fix: Fixes homebrew

--- homebrew/kaytu.rb
+++ homebrew/kaytu.rb
@@ -7,37 +7,21 @@ class Kaytu < Formula
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_darwin_amd64.tar.gz"
     sha256 "HASH_MAC_AMD64"
-    def install
-      system "tar", "-xf", "kaytu_VERSION_HOMEBREW_darwin_amd64.tar.gz"
-      bin.install "kaytu_VERSION_HOMEBREW_darwin_amd64" => "kaytu"
-    end
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_darwin_arm64.tar.gz"
     sha256 "HASH_MAC_ARM64"
-    def install
-      system "tar", "-xf", "kaytu_VERSION_HOMEBREW_darwin_arm64.tar.gz"
-      bin.install "kaytu_VERSION_HOMEBREW_darwin_arm64" => "kaytu"
-    end
   end
 
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_linux_arm64.tar.gz"
     sha256 "HASH_LINUX_ARM64"
-    def install
-      system "tar", "-xf", "kaytu_VERSION_HOMEBREW_linux_arm64.tar.gz"
-      bin.install "kaytu_VERSION_HOMEBREW_linux_arm64" => "kaytu"
-    end
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/kaytu-io/kaytu/releases/download/vVERSION_HOMEBREW/kaytu_VERSION_HOMEBREW_linux_amd64.tar.gz"
     sha256 "HASH_LINUX_AMD64"
-    def install
-      system "tar", "-xf", "kaytu_VERSION_HOMEBREW_linux_amd64.tar.gz"
-      bin.install "kaytu_VERSION_HOMEBREW_linux_amd64" => "kaytu"
-    end
   end
 
 
