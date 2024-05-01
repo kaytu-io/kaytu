3752e690257833e76bd897fefc9841852a308e72
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-05-01 13:55:33 +0400

    refactor: Moving cloud api calls under interface

---
+++ cmd/optimize/view/process.go
@@ -0,0 +1,330 @@
+package view
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	types2 "github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	preferences2 "github.com/kaytu-io/kaytu/cmd/optimize/preferences"
+	"github.com/kaytu-io/kaytu/pkg/api/wastage"
+	"github.com/kaytu-io/kaytu/pkg/hash"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+func (m *App) ProcessAllRegions() {
+	defer func() {
+		if r := recover(); r != nil {
+			m.jobs.PublishError(fmt.Errorf("%v", r))
+		}
+	}()
+
+	job := m.jobs.Publish(Job{ID: "list_all_regions", Descrption: "Listing all available regions"})
+	job.Done = true
+	regions, err := m.provider.ListAllRegions()
+	if err != nil {
+		job.FailureMessage = err.Error()
+		m.jobs.Publish(job)
+		return
+	}
+	m.jobs.Publish(job)
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(regions))
+	for _, region := range regions {
+		region := region
+		go func() {
+			defer wg.Done()
+			m.ProcessRegion(region)
+		}()
+	}
+	wg.Wait()
+}
+
+func (m *App) ProcessRegion(region string) {
+	defer func() {
+		if r := recover(); r != nil {
+			m.jobs.PublishError(fmt.Errorf("%v", r))
+		}
+	}()
+
+	job := m.jobs.Publish(Job{ID: fmt.Sprintf("region_ec2_instances_%s", region), Descrption: "Listing all ec2 instances in " + region})
+	job.Done = true
+
+	instances, err := m.provider.ListInstances(region)
+	if err != nil {
+		job.FailureMessage = err.Error()
+		m.jobs.Publish(job)
+		return
+	}
+	m.jobs.Publish(job)
+
+	for _, instance := range instances {
+		// just to show the loading
+		m.optimizationsTable.SendItem(OptimizationItem{
+			Instance:            instance,
+			Region:              region,
+			OptimizationLoading: true,
+			Preferences:         preferences2.DefaultPreferences(),
+		})
+	}
+
+	for _, instance := range instances {
+		vjob := m.jobs.Publish(Job{ID: fmt.Sprintf("volumes_%s", *instance.InstanceId), Descrption: fmt.Sprintf("getting volumes of %s", *instance.InstanceId)})
+		vjob.Done = true
+
+		volumes, err := m.provider.ListAttachedVolumes(region, instance)
+		if err != nil {
+			vjob.FailureMessage = err.Error()
+			m.jobs.Publish(vjob)
+			return
+		}
+		m.jobs.Publish(vjob)
+
+		imjob := m.jobs.Publish(Job{ID: fmt.Sprintf("instance_%s_metrics", *instance.InstanceId), Descrption: fmt.Sprintf("getting metrics of %s", *instance.InstanceId)})
+		imjob.Done = true
+		startTime := time.Now().Add(-24 * 7 * time.Hour)
+		endTime := time.Now()
+		instanceMetrics := map[string][]types2.Datapoint{}
+		cwMetrics, err := m.metricProvider.GetMetrics(
+			region,
+			"AWS/EC2",
+			[]string{
+				"CPUUtilization",
+				"NetworkIn",
+				"NetworkOut",
+			},
+			map[string][]string{
+				"InstanceId": {*instance.InstanceId},
+			},
+			startTime, endTime,
+			time.Hour,
+			[]types2.Statistic{
+				types2.StatisticAverage,
+				types2.StatisticMaximum,
+			},
+		)
+		if err != nil {
+			imjob.FailureMessage = err.Error()
+			m.jobs.Publish(imjob)
+			return
+		}
+		for k, v := range cwMetrics {
+			instanceMetrics[k] = v
+		}
+
+		cwaMetrics, err := m.metricProvider.GetMetrics(
+			region,
+			"CWAgent",
+			[]string{
+				"mem_used_percent",
+			},
+			map[string][]string{
+				"InstanceId": {*instance.InstanceId},
+			},
+			startTime, endTime,
+			time.Hour,
+			[]types2.Statistic{
+				types2.StatisticAverage,
+				types2.StatisticMaximum,
+			},
+		)
+		if err != nil {
+			imjob.FailureMessage = err.Error()
+			m.jobs.Publish(imjob)
+			return
+		}
+		for k, v := range cwaMetrics {
+			instanceMetrics[k] = v
+		}
+		m.jobs.Publish(imjob)
+
+		ivjob := m.jobs.Publish(Job{ID: fmt.Sprintf("volume_%s_metrics", *instance.InstanceId), Descrption: fmt.Sprintf("getting volume metrics of %s", *instance.InstanceId)})
+		ivjob.Done = true
+
+		var volumeIDs []string
+		for _, v := range instance.BlockDeviceMappings {
+			if v.Ebs != nil {
+				volumeIDs = append(volumeIDs, *v.Ebs.VolumeId)
+			}
+		}
+
+		volumeMetrics := map[string]map[string][]types2.Datapoint{}
+		for _, v := range volumeIDs {
+			volumeMetric, err := m.metricProvider.GetMetrics(
+				region,
+				"AWS/EBS",
+				[]string{
+					"VolumeReadOps",
+					"VolumeWriteOps",
+					"VolumeReadBytes",
+					"VolumeWriteBytes",
+				},
+				map[string][]string{
+					"VolumeId": {v},
+				},
+				startTime, endTime,
+				time.Hour,
+				[]types2.Statistic{
+					types2.StatisticAverage,
+					types2.StatisticMaximum,
+				},
+			)
+			if err != nil {
+				ivjob.FailureMessage = err.Error()
+				m.jobs.Publish(ivjob)
+				return
+			}
+			volumeMetrics[v] = volumeMetric
+		}
+		m.jobs.Publish(ivjob)
+
+		isAutoScaling := false
+		for _, tag := range instance.Tags {
+			if *tag.Key == "aws:autoscaling:groupName" && tag.Value != nil && *tag.Value != "" {
+				isAutoScaling = true
+			}
+		}
+
+		oi := OptimizationItem{
+			Instance:            instance,
+			Volumes:             volumes,
+			Metrics:             instanceMetrics,
+			VolumeMetrics:       volumeMetrics,
+			Region:              region,
+			OptimizationLoading: true,
+			Preferences:         preferences2.DefaultPreferences(),
+		}
+		if instance.State.Name != types.InstanceStateNameRunning ||
+			instance.InstanceLifecycle == types.InstanceLifecycleTypeSpot ||
+			isAutoScaling {
+			oi.OptimizationLoading = false
+			oi.Skipped = true
+			reason := ""
+			if instance.State.Name != types.InstanceStateNameRunning {
+				reason = "not running"
+			} else if instance.InstanceLifecycle == types.InstanceLifecycleTypeSpot {
+				reason = "spot instance"
+			} else if isAutoScaling {
+				reason = "auto-scaling group instance"
+			}
+			if len(reason) > 0 {
+				oi.SkipReason = &reason
+			}
+		}
+		m.optimizationsTable.SendItem(oi)
+		if !oi.Skipped {
+			m.processInstanceChan <- oi
+		}
+	}
+}
+
+func (m *App) ProcessWastages() {
+	for item := range m.processInstanceChan {
+		go m.WastageWorker(item)
+	}
+}
+
+func (m *App) WastageWorker(item OptimizationItem) {
+	defer func() {
+		if r := recover(); r != nil {
+			m.jobs.PublishError(fmt.Errorf("%v", r))
+		}
+	}()
+
+	job := m.jobs.Publish(Job{ID: fmt.Sprintf("wastage_%s", *item.Instance.InstanceId), Descrption: fmt.Sprintf("Evaluating usage data for %s", *item.Instance.InstanceId)})
+	job.Done = true
+
+	var monitoring *types.MonitoringState
+	if item.Instance.Monitoring != nil {
+		monitoring = &item.Instance.Monitoring.State
+	}
+	var placement *wastage.EC2Placement
+	if item.Instance.Placement != nil {
+		placement = &wastage.EC2Placement{
+			Tenancy: item.Instance.Placement.Tenancy,
+		}
+		if item.Instance.Placement.AvailabilityZone != nil {
+			placement.AvailabilityZone = *item.Instance.Placement.AvailabilityZone
+		}
+		if item.Instance.Placement.HostId != nil {
+			placement.HashedHostId = hash.HashString(*item.Instance.Placement.HostId)
+		}
+	}
+	platform := ""
+	if item.Instance.PlatformDetails != nil {
+		platform = *item.Instance.PlatformDetails
+	}
+
+	var volumes []wastage.EC2Volume
+	for _, v := range item.Volumes {
+		volumes = append(volumes, toEBSVolume(v))
+	}
+
+	res, err := wastage.Ec2InstanceWastageRequest(wastage.EC2InstanceWastageRequest{
+		Identification: m.identification,
+		Instance: wastage.EC2Instance{
+			HashedInstanceId:  hash.HashString(*item.Instance.InstanceId),
+			State:             item.Instance.State.Name,
+			InstanceType:      item.Instance.InstanceType,
+			Platform:          platform,
+			ThreadsPerCore:    *item.Instance.CpuOptions.ThreadsPerCore,
+			CoreCount:         *item.Instance.CpuOptions.CoreCount,
+			EbsOptimized:      *item.Instance.EbsOptimized,
+			InstanceLifecycle: item.Instance.InstanceLifecycle,
+			Monitoring:        monitoring,
+			Placement:         placement,
+			UsageOperation:    *item.Instance.UsageOperation,
+			Tenancy:           item.Instance.Placement.Tenancy,
+		},
+		Volumes:       volumes,
+		Metrics:       item.Metrics,
+		VolumeMetrics: item.VolumeMetrics,
+		Region:        item.Region,
+		Preferences:   preferences2.Export(item.Preferences),
+	})
+	if err != nil {
+		if strings.Contains(err.Error(), "please login") {
+			fmt.Println(err.Error())
+			os.Exit(1)
+			return
+		}
+		job.FailureMessage = err.Error()
+		m.jobs.Publish(job)
+		return
+	}
+	m.jobs.Publish(job)
+
+	if res.RightSizing.Current.InstanceType == "" {
+		item.OptimizationLoading = false
+		m.optimizationsTable.SendItem(item)
+		return
+	}
+
+	m.optimizationsTable.SendItem(OptimizationItem{
+		Instance:            item.Instance,
+		Volumes:             item.Volumes,
+		Region:              item.Region,
+		OptimizationLoading: false,
+		Wastage:             *res,
+		Preferences:         item.Preferences,
+	})
+}
+
+func toEBSVolume(v types.Volume) wastage.EC2Volume {
+	var throughput *float64
+	if v.Throughput != nil {
+		throughput = aws.Float64(float64(*v.Throughput))
+	}
+
+	return wastage.EC2Volume{
+		HashedVolumeId:   hash.HashString(*v.VolumeId),
+		VolumeType:       v.VolumeType,
+		Size:             v.Size,
+		Iops:             v.Iops,
+		AvailabilityZone: v.AvailabilityZone,
+		Throughput:       throughput,
+	}
+}
---
+++ pkg/provider/aws.go
@@ -0,0 +1,117 @@
+package provider
+
+import (
+	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/organizations"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+type AWS struct {
+	cfg aws.Config
+}
+
+func NewAWS(cfg aws.Config) (*AWS, error) {
+	return &AWS{cfg: cfg}, nil
+}
+
+func (s *AWS) Identify() (map[string]string, error) {
+	client := sts.NewFromConfig(s.cfg)
+	out, err := client.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
+	if err != nil {
+		return nil, errors.New("unable to retrieve AWS account details, please check your AWS cli and ensure that you are logged-in")
+	}
+
+	orgClient := organizations.NewFromConfig(s.cfg)
+	orgOut, _ := orgClient.DescribeOrganization(context.Background(), &organizations.DescribeOrganizationInput{})
+
+	identification := map[string]string{}
+	identification["account"] = *out.Account
+	identification["user_id"] = *out.UserId
+	identification["sts_arn"] = *out.Arn
+
+	if orgOut != nil && orgOut.Organization != nil {
+		identification["org_id"] = *orgOut.Organization.Id
+		identification["org_m_arn"] = *orgOut.Organization.MasterAccountArn
+		identification["org_m_email"] = *orgOut.Organization.MasterAccountEmail
+		identification["org_m_account"] = *orgOut.Organization.MasterAccountId
+	}
+	return identification, nil
+}
+
+func (s *AWS) ListAllRegions() ([]string, error) {
+	regionClient := ec2.NewFromConfig(s.cfg)
+	regions, err := regionClient.DescribeRegions(context.Background(), &ec2.DescribeRegionsInput{AllRegions: aws.Bool(false)})
+	if err != nil {
+		return nil, err
+	}
+	var regionCodes []string
+	for _, r := range regions.Regions {
+		regionCodes = append(regionCodes, *r.RegionName)
+	}
+	return regionCodes, nil
+}
+
+func (s *AWS) ListInstances(region string) ([]types.Instance, error) {
+	localCfg := s.cfg
+	localCfg.Region = region
+
+	var vms []types.Instance
+	ctx := context.Background()
+	client := ec2.NewFromConfig(localCfg)
+	paginator := ec2.NewDescribeInstancesPaginator(client, &ec2.DescribeInstancesInput{})
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, r := range page.Reservations {
+			for _, v := range r.Instances {
+				if v.State.Name != types.InstanceStateNameRunning {
+					continue
+				}
+				if v.InstanceLifecycle == types.InstanceLifecycleTypeSpot {
+					continue
+				}
+				isAutoScaling := false
+				for _, tag := range v.Tags {
+					if *tag.Key == "aws:autoscaling:groupName" && tag.Value != nil && *tag.Value != "" {
+						isAutoScaling = true
+					}
+				}
+				if isAutoScaling {
+					continue
+				}
+
+				vms = append(vms, v)
+			}
+		}
+	}
+	return vms, nil
+}
+
+func (s *AWS) ListAttachedVolumes(region string, instance types.Instance) ([]types.Volume, error) {
+	localCfg := s.cfg
+	localCfg.Region = region
+
+	var volumeIDs []string
+	for _, v := range instance.BlockDeviceMappings {
+		if v.Ebs != nil {
+			volumeIDs = append(volumeIDs, *v.Ebs.VolumeId)
+		}
+	}
+
+	client := ec2.NewFromConfig(localCfg)
+	volumesResp, err := client.DescribeVolumes(context.Background(), &ec2.DescribeVolumesInput{
+		VolumeIds: volumeIDs,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return volumesResp.Volumes, nil
+}
