6694caface62932b89bab4fcf44891380488916c
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-05-09 01:26:28 +0400

    fix: Fixes home folder for windows

--- pkg/server/plugin.go
+++ pkg/server/plugin.go
@@ -6,14 +6,14 @@ import (
 )
 
 func PluginDir() string {
-	home := os.Getenv("HOME")
+	home := Home()
 	dir := fmt.Sprintf("%s/.kaytu/plugins/", home)
 	os.MkdirAll(dir, os.ModePerm)
 	return dir
 }
 
 func LogsDir() string {
-	home := os.Getenv("HOME")
+	home := Home()
 	dir := fmt.Sprintf("%s/.kaytu/logs", home)
 	os.MkdirAll(dir, os.ModePerm)
 	return dir
