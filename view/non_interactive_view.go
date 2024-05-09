013a9dd3a39c8f79c1177ea6113a19c6f2679e3d
artaasadi <arta-c@kaytu.io>
2024-05-09 18:29:57 +0200

    fix: non-interactive mode finished

--- view/non_interactive_view.go
+++ view/non_interactive_view.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/kaytu-io/kaytu/pkg/plugin/proto/src/golang"
-	"sync"
 	"time"
 )
 
@@ -14,45 +13,36 @@ type NonInteractiveView struct {
 	itemsChan chan *golang.OptimizationItem
 	items     []*golang.OptimizationItem
 
-	runningJobsMap map[string]string
-	failedJobsMap  map[string]string
-	statusErr      string
-
 	errorChan chan error
-
-	jobChan  chan *golang.JobResult
-	jobs     []*golang.JobResult
-	jobMutex sync.RWMutex
 
 	resultsReady chan bool
 }
 
 func NewNonInteractiveView() *NonInteractiveView {
 	v := &NonInteractiveView{
-		itemsChan:      make(chan *golang.OptimizationItem, 1000),
-		runningJobsMap: map[string]string{},
-		failedJobsMap:  map[string]string{},
-		jobMutex:       sync.RWMutex{},
-		jobChan:        make(chan *golang.JobResult, 10000),
-		errorChan:      make(chan error, 10000),
-		resultsReady:   make(chan bool),
+		itemsChan:    make(chan *golang.OptimizationItem, 1000),
+		errorChan:    make(chan error, 10000),
+		resultsReady: make(chan bool),
 	}
-	v.resultsReady <- false
 	return v
 }
 
-var primary = color.New(color.FgHiCyan)
-
 var bold = color.New(color.Bold)
-var faint = color.New(color.Faint)
 var underline = color.New(color.Underline)
-
-var primaryLink = color.New(color.Underline).Add(color.Bold)
 
 // OptimizationsString returns a string to show the optimization results and details
 func (v *NonInteractiveView) OptimizationsString() (string, error) {
-	var costString string
+	var resultsString string
 
+	for _, item := range v.items {
+		resultsString += getItemString(item)
+		resultsString += "\n──────────────────────────────────\n"
+	}
+
+	return resultsString, nil
+}
+
+func getItemString(item *golang.OptimizationItem) string {
 	t := table.NewWriter()
 	t.Style().Options.DrawBorder = false
 	t.Style().Options.SeparateColumns = false
@@ -101,13 +91,15 @@ func (v *NonInteractiveView) OptimizationsString() (string, error) {
 	})
 	i++
 
-	t.AppendRow(table.Row{""})
-
 	t.SetColumnConfigs(columns)
 	t.AppendHeader(headers)
-
-	for _, item := range v.items {
-		var row table.Row
+	var row table.Row
+	var itemString string
+	if item.Skipped {
+		row = append(row, item.Id, item.ResourceType, item.Region, item.Platform, "Row Skipped")
+		t.AppendRow(row)
+		itemString += t.Render()
+	} else {
 		totalSaving := 0.0
 		if !item.Loading && !item.Skipped && !item.LazyLoadingEnabled {
 			for _, dev := range item.Devices {
@@ -115,22 +107,150 @@ func (v *NonInteractiveView) OptimizationsString() (string, error) {
 			}
 		}
 		row = append(row, item.Id, item.ResourceType, item.Region, item.Platform, fmt.Sprintf("$%.2f", totalSaving))
-
 		t.AppendRow(row)
+		itemString += t.Render()
+		itemString += "\n    " + bold.Sprint("Devices") + ":"
+		for _, dev := range item.Devices {
+			itemString += "\n"
+			itemString += getDeviceString(dev)
+		}
 	}
 
-	costString = t.Render()
-	costString += "\n──────────────────────────────────\n"
+	return itemString
+}
 
-	return costString, nil
+func getDeviceString(dev *golang.Device) string {
+	t := table.NewWriter()
+	t.Style().Options.DrawBorder = false
+	t.Style().Options.SeparateColumns = false
+	t.Style().Options.SeparateRows = false
+	t.Style().Options.SeparateHeader = false
+	t.Style().Format.Header = text.FormatDefault
+
+	var columns []table.ColumnConfig
+	i := 1
+	var headers table.Row
+	headers = append(headers, underline.Sprint(""))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("ResourceType"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Runtime"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Current Cost"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Right Sized Cost"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignRight,
+		AlignHeader: text.AlignRight,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Savings"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignRight,
+		AlignHeader: text.AlignRight,
+	})
+	i++
+
+	t.SetColumnConfigs(columns)
+	t.AppendHeader(headers)
+	var row table.Row
+	var itemString string
+	row = append(row, "└─ "+dev.DeviceId, dev.ResourceType, dev.Runtime, dev.CurrentCost, dev.RightSizedCost, fmt.Sprintf("$%.2f", dev.CurrentCost-dev.RightSizedCost))
+	t.AppendRow(row)
+	itemString += t.Render()
+	itemString += "\n        " + bold.Sprint("Properties") + ":\n" + getPropertiesString(dev.Properties)
+	return itemString
+}
+
+func getPropertiesString(properties []*golang.Property) string {
+	t := table.NewWriter()
+	t.Style().Options.DrawBorder = false
+	t.Style().Options.SeparateColumns = false
+	t.Style().Options.SeparateRows = false
+	t.Style().Options.SeparateHeader = false
+	t.Style().Format.Header = text.FormatDefault
+
+	var columns []table.ColumnConfig
+	i := 1
+	var headers table.Row
+	headers = append(headers, underline.Sprint(""))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Current"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Average Usage"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Max Usage"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Recommendation"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignRight,
+		AlignHeader: text.AlignRight,
+	})
+	i++
+
+	t.SetColumnConfigs(columns)
+	t.AppendHeader(headers)
+
+	var itemString string
+	for _, p := range properties {
+		var row table.Row
+		row = append(row, "└───── "+p.Key, p.Current, p.Average, p.Max, p.Recommended)
+		t.AppendRow(row)
+	}
+	itemString += t.Render()
+	return itemString
 }
 
 func (v *NonInteractiveView) PublishItem(item *golang.OptimizationItem) {
 	v.itemsChan <- item
-}
-
-func (v *NonInteractiveView) PublishJob(job *golang.JobResult) {
-	v.jobChan <- job
 }
 
 func (v *NonInteractiveView) PublishError(err error) {
@@ -142,21 +262,23 @@ func (v *NonInteractiveView) PublishResultsReady(ready *golang.ResultsReady) {
 }
 
 func (v *NonInteractiveView) WaitAndShowResults() error {
-	go v.WaitForJobs()
 	go v.WaitForAllItems()
-	for len(v.runningJobsMap) == 0 {
-		time.Sleep(500 * time.Millisecond)
+	for {
+		select {
+		case ready := <-v.resultsReady:
+			if ready == true {
+				str, err := v.OptimizationsString()
+				if err != nil {
+					return err
+				}
+				fmt.Println(str)
+				return nil
+			}
+		case err := <-v.errorChan:
+			fmt.Println(err.Error())
+			return nil
+		}
 	}
-	time.Sleep(5 * time.Second)
-	for v.itemLoadingExists() && len(v.runningJobsMap) > 0 {
-		time.Sleep(5 * time.Second)
-	}
-	str, err := v.OptimizationsString()
-	if err != nil {
-		return err
-	}
-	fmt.Println(str)
-	return nil
 }
 
 func (v *NonInteractiveView) itemLoadingExists() bool {
@@ -166,39 +288,6 @@ func (v *NonInteractiveView) itemLoadingExists() bool {
 		}
 	}
 	return false
-}
-
-func (v *NonInteractiveView) jobPendingExists() bool {
-	for _, j := range v.jobs {
-		if j.Done == false {
-			return true
-		}
-	}
-	return false
-}
-
-func (v *NonInteractiveView) WaitForJobs() {
-	for {
-		select {
-		case job := <-v.jobChan:
-			v.jobMutex.Lock()
-			if !job.Done {
-				v.runningJobsMap[job.Id] = job.Description
-			} else {
-				if _, ok := v.runningJobsMap[job.Id]; ok {
-					delete(v.runningJobsMap, job.Id)
-				}
-			}
-			if len(job.FailureMessage) > 0 {
-				v.failedJobsMap[job.Id] = fmt.Sprintf("%s failed due to %s", job.Description, job.FailureMessage)
-			}
-			v.jobMutex.Unlock()
-
-		case err := <-v.errorChan:
-			v.statusErr = fmt.Sprintf("Failed due to %v", err)
-		}
-		time.Sleep(50 * time.Millisecond)
-	}
 }
 
 func (v *NonInteractiveView) WaitForAllItems() {
