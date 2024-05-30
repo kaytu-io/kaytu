de348f26725fb54011f224d4c20b7f94d9ca9926
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-05-30 17:31:59 +0400

    feat: add plugin custom charts (#169)
    * feat: add plugin custom charts
    
    * fix: overview chart replacement problem

--- controller/optimization.go
+++ controller/optimization.go
@@ -4,31 +4,42 @@ import (
 	"github.com/kaytu-io/kaytu/pkg/plugin/proto/src/golang"
 )
 
-type Optimizations struct {
-	itemsChan chan *golang.OptimizationItem
-	items     []*golang.OptimizationItem
+type Optimizations[T golang.OptimizationItem | golang.ChartOptimizationItem] struct {
+	itemsChan chan *T
+	items     []*T
 
-	selectedItem *golang.OptimizationItem
+	selectedItem *T
 
 	reEvaluateFunc func(id string, items []*golang.PreferenceItem)
 }
 
-func NewOptimizations() *Optimizations {
-	o := Optimizations{
-		itemsChan: make(chan *golang.OptimizationItem, 1000),
+func NewOptimizations[T golang.OptimizationItem | golang.ChartOptimizationItem]() *Optimizations[T] {
+	o := Optimizations[T]{
+		itemsChan: make(chan *T, 1000),
 	}
 	go o.Process()
 	return &o
 }
 
-func (o *Optimizations) Process() {
+func (o *Optimizations[T]) Process() {
 	for newItem := range o.itemsChan {
 		updated := false
 		for idx, i := range o.items {
-			if newItem.Id == i.Id {
-				o.items[idx] = newItem
-				updated = true
-				break
+			switch castedNewItem := any(newItem).(type) {
+			case *golang.OptimizationItem:
+				castedI := any(i).(*golang.OptimizationItem)
+				if castedNewItem.Id == castedI.Id {
+					o.items[idx] = newItem
+					updated = true
+					break
+				}
+			case *golang.ChartOptimizationItem:
+				castedI := any(i).(*golang.ChartOptimizationItem)
+				if castedNewItem.GetOverviewChartRow().GetRowId() == castedI.GetOverviewChartRow().GetRowId() {
+					o.items[idx] = newItem
+					updated = true
+					break
+				}
 			}
 		}
 		if !updated {
@@ -37,26 +48,26 @@ func (o *Optimizations) Process() {
 	}
 }
 
-func (o *Optimizations) SendItem(item *golang.OptimizationItem) {
+func (o *Optimizations[T]) SendItem(item *T) {
 	o.itemsChan <- item
 }
 
-func (o *Optimizations) Items() []*golang.OptimizationItem {
+func (o *Optimizations[T]) Items() []*T {
 	return o.items
 }
 
-func (o *Optimizations) SetReEvaluateFunc(f func(id string, items []*golang.PreferenceItem)) {
+func (o *Optimizations[T]) SetReEvaluateFunc(f func(id string, items []*golang.PreferenceItem)) {
 	o.reEvaluateFunc = f
 }
 
-func (o *Optimizations) SelectItem(i *golang.OptimizationItem) {
+func (o *Optimizations[T]) SelectItem(i *T) {
 	o.selectedItem = i
 }
 
-func (o *Optimizations) SelectedItem() *golang.OptimizationItem {
+func (o *Optimizations[T]) SelectedItem() *T {
 	return o.selectedItem
 }
 
-func (o *Optimizations) ReEvaluate(id string, preferences []*golang.PreferenceItem) {
+func (o *Optimizations[T]) ReEvaluate(id string, preferences []*golang.PreferenceItem) {
 	o.reEvaluateFunc(id, preferences)
 }
