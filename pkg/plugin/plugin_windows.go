6db0cd0627ad6865110c7635069e704c5861bbbe
Mahan Zendedel DH <mahanmmi@gmail.com>
2024-06-23 21:42:31 +0400

    fix: seprate windows

--- pkg/plugin/plugin_windows.go
+++ pkg/plugin/plugin_windows.go
@@ -1,3 +1,6 @@
+//go:build windows
+// +build windows
+
 package plugin
 
 import (
@@ -8,15 +11,11 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"syscall"
 )
 
 func startPlugin(ctx context.Context, plg *server.Plugin, serverAddr string) (*exec.Cmd, error) {
 	logsDir := server.LogsDir()
 	cmd := exec.CommandContext(ctx, plg.Path(), "--server", serverAddr)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Pdeathsig: syscall.SIGKILL,
-	}
 
 	errLogs, err := os.OpenFile(filepath.Join(logsDir, fmt.Sprintf("%s.err.logs", strings.ReplaceAll(plg.Config.Name, "/", "_"))), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
